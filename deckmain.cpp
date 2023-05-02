7f7a751bac066d8ed10f3b176e9bbe49c9e95997
Alex Anderson <chaoticgamer.exe@gmail.com>
2023-05-02 12:19:55 -0400

    most things work fine but get a malloc error for some reason whenever using print

--- deckmain.cpp
+++ deckmain.cpp
@@ -5,8 +5,10 @@ using namespace std;
 
 
 int main() {
-	Deck deck;
-	//deck.shuffle();
+	Deck deck = Deck();
+	//deck.print();
+	deck.shuffle();
+	//deck.print();
 	Deck hand1 = deck.subdeck(0, 4);
 	Deck hand2 = deck.subdeck(5, 9);
 	Deck pack = deck.subdeck(10, 51);
