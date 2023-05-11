7823388457d8c9eb519c7a4bcc89801e8b894fec
Alex_A <77032042+crazininja@users.noreply.github.com>
2023-05-10 21:26:04 -0400

    for loop function exercise completed

--- linkedlists.cpp
+++ linkedlists.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 template <class T>
 class Node
 {
@@ -27,9 +29,39 @@ public:
     }
 };
 
+template <class T>
+void print_list(Node<T>* list) {
+    Node<T>* node= list;
+    while (node != NULL) {
+        cout << node->cargo;
+        node = node->next;
+        if (node != NULL) cout << ", ";
+    }
+}
+
+template <class T>
+void print_for_list(Node<T>* list) {
+    Node<T>* node = list;
+    //safety
+    for (Node<T>* i = node; i != NULL; i = i->next) {
+        cout << i->cargo << endl;
+        if (v > 10) { break; }
+    }
+}
 
 int main(int argc, char** argv)
 {
-	
-	return 0;
+    //textbook tests
+    Node<string>* node1 = new Node<string>("I", NULL);
+    Node<string>* node2 = new Node<string>("love", NULL);
+    Node<string>* node3 = new Node<string>("templates!", NULL);
+
+    node1->next = node2;
+    node2->next = node3;
+
+    Node<string>* node = node1;
+
+    print_for_list(node);
+
+    return 0;
 }
