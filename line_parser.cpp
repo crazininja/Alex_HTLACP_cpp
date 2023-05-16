b82603affb14aee3c70e33067f238da38bc7f9da
Alex Anderson <chaoticgamer.exe@gmail.com>
2023-05-16 12:52:13 -0400

    string input completed

--- line_parser.cpp
+++ line_parser.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
-#include <unordered_map>
+#include <set>
 using namespace std;
 
 //regurgitate a line 
@@ -10,21 +10,13 @@ int main() {
 string input;
 cout << "Enter an expression" << endl;
 getline(cin, input);
-cout <<"input:" << input << " and it's size:" << input.length() << endl;
 vector<char> infix = vector<char>();
-
-
-//for (int i = 0; i < input.length(); i++)  {
-    //treaverse the string
-//    cout << "character going into infix:" << endl;
-//    infix.assign(input.at(i), i);
-//}
-
-//cout << "infix size:" << infix.size() << endl;
-
+set<char> valids = set<char>({'(',')','0','1','2','3','4','5','6','7','8','9','+','-','/','*'});
  for (int i = 0; i < input.size(); i++)  {
-    cout << "pushing back:"<< input[i] << endl;
-    infix.push_back(input[i]);
+    //count will return a 1 or 0
+    if (valids.count(input[i])) {
+        infix.push_back(input[i]);
+    }
  }
 
  for (int i = 0; i < infix.size(); i++)  {
