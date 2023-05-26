04495a797706b4466622c89913eae202c60c64bf
Alex_A <77032042+crazininja@users.noreply.github.com>
2023-05-25 20:20:21 -0400

    random break on insert at for some reason, but rest of program runs

--- linkedlists.cpp
+++ linkedlists.cpp
@@ -52,6 +52,11 @@ public:
         num_nodes++;
     }
 
+    void modify_node(int pos, T value) {
+        Node<T>* node = get_node(pos);
+        node->cargo = value;
+    }
+
     T remove_from_front() {
         if (head == NULL)
             throw runtime_error("Can't remove from empty list!");
@@ -61,6 +66,27 @@ public:
         delete front;
         num_nodes--;
         return cargo;
+    }
+
+    Node<T>* get_node(int pos) {
+        //edge cases
+        if (num_nodes == 0) {
+            return NULL;
+        }
+        if (pos == 0) {
+            return head;
+        }
+        if (pos == num_nodes - 1) { return tail; }
+        if (pos >= num_nodes) { return NULL; }
+        //traverse the list
+        //we can start at next
+        Node<T>* node = head->next;
+        int node_pos = 1;
+        while (node_pos < pos) {
+            node = node->next;
+            node_pos++;
+        }
+        return node;
     }
 
     void insert_at(T value, int pos) {
@@ -89,16 +115,20 @@ public:
         //potential optimization: just use the lag node, but it might be less readable?
 
         //start at the first node so we can have a lag node and traverse
-        Node<T>* node = head->next;
-        Node<T>* lag_node = head;
-        for (int position = 1; (position < pos); position++) {
-            node = node->next;
-            lag_node = lag_node->next;
-        }
+       // Node<T>* node = head->next;
+       // Node<T>* lag_node = head;
+       // for (int position = 1; (position < pos); position++) {
+       //     node = node->next;
+        //    lag_node = lag_node->next;
+        //}
         //node is the node we want to replace
-        cout << "lag node:" << lag_node->cargo << endl;
-        cout << "next node:" << node->cargo << endl;
-        lag_node->next = new Node<T>(value, node);
+        //lag_node->next = new Node<T>(value, node);
+        //num_nodes++;
+         
+        //traverse until the node right before it
+        Node<T>* behind_node = get_node(pos - 1);
+        Node<T>* ahead_node = behind_node->next;
+        behind_node->next = new Node<T>(value, ahead_node);
         num_nodes++;
         return;
     }
@@ -128,6 +158,17 @@ int main(int argc, char** argv)
     one_two_three->insert_at(4, 2);
     one_two_three->insert_at_end(5);
     print_list(one_two_three);
+    cout << " Let's get the two! " << one_two_three->get_node(1)->cargo << endl;
+    cout << " Let's fix the 4 and 3!" << endl;
+    //program seems to end after modify node?
+    one_two_three->modify_node(2, 3);
+    one_two_three->modify_node(3, 4);
+    print_list(one_two_three);
+    cout << endl << "let's insert a 25 after 3!" << endl;
+    one_two_three->insert_at(2, 25);
+    print_list(one_two_three);
+    cout << "Endl of program" << endl;
+
 
 
 
